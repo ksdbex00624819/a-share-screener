ae4f7898716aac4e77217e1bc3f83cd4846e5c92
李可 <like1@huaxiapawn.com>
2026-02-02 17:26:10 +0800

    修改使用的jdk版本为java 21，修改默认测试配置，增加测试数据初始化脚本

--- mvnw21.cmd
+++ mvnw21.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set "JAVA_HOME=E:\Java\jdk-17"
+set "JAVA_HOME=E:\Java\jdk-21.0.10"
 set "PATH=%JAVA_HOME%\bin;%PATH%"
 
 call mvnw.cmd %*
---
+++ src/test/resources/data-test.sql
@@ -0,0 +1,2 @@
+INSERT INTO stock_basic (code, name, market, secid, board, status, created_at, updated_at)
+VALUES ('000001', '平安银行', 0, '0.000001', 'MAIN', 'ACTIVE', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
